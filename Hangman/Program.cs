835764c1fa47a7dcc0bc466cf37e2aaf8e6dfde3
hermben <hermben@gmail.com>
2021-09-13 16:30:45 -0700

    adds logic to program.cs

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -6,8 +6,53 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wlecome to Hangman");
+            string[] listwords = new string[3];
+            listwords[0] = "beautiful";
+            listwords[1] = "happy";
+            listwords[2] = "boy";
 
+
+            var won = false;
+            Random wordRand = new Random();
+            var exo = wordRand.Next(0, 3);
+            string wordToGuess = listwords[exo];
+            char[] guess = new char[wordToGuess.Length];
+
+            Console.Write("Please enter your guess");
+
+            for (int s = 0; s < wordToGuess.Length; s++)
+                guess[s] = '-';
+
+            while (won == false )
+            {
+                Console.WriteLine("please enter a letter: ");
+
+                char guessChar =Convert.ToChar( Console.ReadLine());
+
+                for (int i =0; i< wordToGuess.Length; i++)
+                {
+                    if (guessChar == wordToGuess[i])
+                    {
+                        guess[i] = guessChar;
+                    }
+
+                }
+                Console.WriteLine(guess);
+                // verifies if user won
+                won = true;
+                for (int j = 0;j<guess.Length; j++)
+                {
+                    if (guess[j]== '-')
+                    {
+                        won = false;
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine("you have won");
 
         }
     }
--- Hangman/WordList.cs
+++ Hangman/WordList.cs
@@ -8,16 +8,8 @@ namespace Hangman
 {
     class WordList
     {
-        string[] listwords = new string[10];
-        listwords[0] = "beautiful";
-            listwords[1] = "happy";
-            listwords[2] = "boy";
 
-            Random wordRand = new Random(0, 2);
-        var exo = wordRand.Next(0, 9);
-        string wordToGuess = listwords[exo];
-            char[]guess= new char[]
-
+       
      }
 
 }
