d579ace264551536fc57cff6a38b23b73ca811d8
Herman Benao <hermben@gmail.com>
2021-09-13 09:43:47 -0700

    adds .gitignore file

---
+++ Hangman/WordList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    class WordList
+    {
+        string[] listwords = new string[10];
+            listwords[0] = "beautiful";
+            listwords[1] = "happy";
+            listwords[2] = "boy";
+
+    }
+    public string
+}
